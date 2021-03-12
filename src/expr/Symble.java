5ab5305e00ba3a88f062223f4a86d262b0fde04e
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 14:38:57 -0300

    Creation of the Symble class.

---
+++ src/expr/Symble.java
@@ -0,0 +1,37 @@
+package expr;
+
+public class Symble {
+    private String id;
+    private Integer value;
+    private String type;
+
+    public Symble(String _id, Integer _value, String _type) {
+        this.id = _id;
+        this.value = _value;
+        this.type = _type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
