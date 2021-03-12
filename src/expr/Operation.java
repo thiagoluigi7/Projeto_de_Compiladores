f3a0b8f75cf8864dac1f4381ef15a1745597701e
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 15:35:45 -0300

    :sparkles: Getters and Setters and the inclusion of an ID.

--- src/expr/Operation.java
+++ src/expr/Operation.java
@@ -4,6 +4,7 @@ public class Operation extends Expression {
     Attribution operand1;
     Attribution operand2;
     char operator;
+    Id id;
 
     public Operation(Attribution _operand1, Attribution _operand2, char _operator) {
         this.operand1 = _operand1;
@@ -11,8 +12,41 @@ public class Operation extends Expression {
         this.operator = _operator;
     }
 
+    public Attribution getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(Attribution operand1) {
+        this.operand1 = operand1;
+    }
+
+    public Attribution getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(Attribution operand2) {
+        this.operand2 = operand2;
+    }
+
+    public char getOperator() {
+        return operator;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    public void setId(Id id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
-        return operand1 + "" + operator + operand2;
+        return operand1 + " " + operator + " " + operand2;
     }
+
 }
