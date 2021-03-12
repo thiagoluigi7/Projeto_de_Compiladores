7590e6968313df50aa26793b43522f86d7a8404c
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 16:29:00 -0300

    :sparkles: This class now has a getter.

--- src/expr/Number.java
+++ src/expr/Number.java
@@ -3,9 +3,12 @@ package expr;
 public class Number extends Expression{
     int value;
 
-
     public Number(int value){
         this.value=value;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     @Override
