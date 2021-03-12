eb61bb8edb0d18d34cc51de9e57717a3e4cccef8
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 19:49:03 -0300

    :memo: Added javadoc comments.

--- src/expr/SymbolTable.java
+++ src/expr/SymbolTable.java
@@ -2,6 +2,10 @@ package expr;
 
 import java.util.ArrayList;
 
+/**
+ * Class used to store all the attributions of the program.
+ * It consists of an ArrayList<Attribution>.
+ */
 public class SymbolTable {
 
     private ArrayList<Attribution> symbolTable;
@@ -14,6 +18,16 @@ public class SymbolTable {
         symbolTable.add(_attribution);
     }
 
+    /**
+     * This method iterates over the array looking for the element
+     * that have the given id. Once this element is found this method
+     * returns its value.
+     * I.e: Consider: a:INT=1; Given 'a' as the id this method will return 1.
+     * @param id id is the name of the variable.
+     * @return Returns the value inside the given variable
+     * @throws NullPointerException If the given id can't be found this method
+     * throws a NullPointerException showing that the given id doesn't exist.
+     */
     public int valueof(Id id) {
         for (int i = 0; i < symbolTable.size(); i++) {
             if (symbolTable.get(i).getId().equals(id)) {
@@ -23,6 +37,11 @@ public class SymbolTable {
         throw new NullPointerException("Value not found.");
     }
 
+    /**
+     * Boolean method to check if a given id exists or not.
+     * @param id id is the name of the variable
+     * @return Returns true if the id exists or false if it doesn't
+     */
     public boolean check(Id id) {
         for (int i = 0; i < symbolTable.size(); i++) {
             if (symbolTable.get(i).getId().equals(id)) {
