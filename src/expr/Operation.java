65021cf51f1f55997663ea7ac0cd1ebbfc74e588
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 15:17:05 -0300

    :pencil2: Typo fix.

--- src/expr/Operation.java
+++ src/expr/Operation.java
@@ -5,10 +5,10 @@ public class Operation extends Expression {
     Atribuicao operand2;
     char operator;
 
-    public Operation(Atribuicao _operando1, Atribuicao _operando2, char _operador) {
-        this.operand1 = _operando1;
-        this.operand2 = _operando2;
-        this.operator = _operador;
+    public Operation(Atribuicao _operand1, Atribuicao _operand2, char _operator) {
+        this.operand1 = _operand1;
+        this.operand2 = _operand2;
+        this.operator = _operator;
     }
 
     @Override
