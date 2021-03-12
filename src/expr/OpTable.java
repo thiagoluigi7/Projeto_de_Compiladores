872c635dcf18b2fb670b8f2851b56775fd5bacac
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 19:51:13 -0300

    :memo: Added javadoc comments.

--- src/expr/OpTable.java
+++ src/expr/OpTable.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class containing an ArrayList with all the operations of the program.
+ */
 public class OpTable {
 
     private ArrayList<Operation> opTable;
@@ -16,15 +19,31 @@ public class OpTable {
         opTable.add(_operation);
     }
 
-    public int valueof(String id) {
+    //Todo Improve this comment in the 24 line.
+    /**
+     * This method iterates over the array looking for the operation
+     * that have the given id. Once this operation is found this method
+     * returns the operation in a readable way.
+     * I.e: Consider: a:INT=1; Given 'a' as the id this method will return 1.
+     * @param id id is the name of the operation.
+     * @return Returns operation in a readable way
+     * @throws NullPointerException If the given id can't be found this method
+     * throws a NullPointerException showing that the given operation doesn't exist.
+     */
+    public int valueof(Id id) {
         for (int i = 0; i < opTable.size(); i++) {
-            if (opTable.get(i).getId().getName().equals(id)) {
+            if (opTable.get(i).getId().equals(id)) {
                 System.out.println(opTable.get(i));
             }
         }
         throw new NullPointerException("Operation not found.");
     }
 
+    /**
+     * Boolean method to check if the operation that contains the given id exists or not.
+     * @param id id is the id of the operation
+     * @return Returns true if the operation exists or false if it doesn't
+     */
     public boolean check(String id) {
         for (int i = 0; i < opTable.size(); i++) {
             if (opTable.get(i).getId().getName().equals(id)) {
