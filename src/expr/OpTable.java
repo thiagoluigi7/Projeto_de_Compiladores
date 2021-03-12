4565e21692d9677ba16b69f1f5e222afc9857415
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 15:41:01 -0300

    :recycle: Adaptation of the SymbolTable to be used as OperationTable (OpTable).

--- src/expr/OpTable.java
+++ src/expr/OpTable.java
@@ -1,30 +1,40 @@
 package expr;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class OpTable {
 
-    public Map<String, Integer> symbleTable;
+    private ArrayList<Operation> opTable;
 
     public OpTable() {
-        this.symbleTable = new HashMap<>();
+        this.opTable = new ArrayList<>();
     }
 
-    public void add(String id, int value){
-        symbleTable.put(id,value);
+    public void add(Operation _operation) {
+        opTable.add(_operation);
     }
 
-    public int valueof(String id){
-        return symbleTable.get(id);
+    public int valueof(String id) {
+        for (int i = 0; i < opTable.size(); i++) {
+            if (opTable.get(i).getId().getName().equals(id)) {
+                System.out.println(opTable.get(i));
+            }
+        }
+        throw new NullPointerException("Operation not found.");
     }
 
-    public boolean check(String id){
-        if (symbleTable.containsKey(id)) return true;
-        else return false;
+    public boolean check(String id) {
+        for (int i = 0; i < opTable.size(); i++) {
+            if (opTable.get(i).getId().getName().equals(id)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void print(){
-        System.out.println(symbleTable.toString());
+        System.out.println(opTable.toString());
     }
 }
