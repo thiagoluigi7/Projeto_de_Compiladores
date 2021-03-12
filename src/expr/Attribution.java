c085d912cb8837a771d83cfe09022ebb3b581d1d
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 16:06:29 -0300

    :construction: Adaptation of the Attribution class.

--- src/expr/Attribution.java
+++ src/expr/Attribution.java
@@ -5,9 +5,34 @@ public class Attribution extends Expression {
     Number value;
     Type type;
 
-    public Attribution(String id, int value) {
-        this.id = new Id(id);
-        this.value = new Number(value);
+    public Attribution(Id _id, Number _value, Type _type) {
+        this.id = _id;
+        this.value = _value;
+        this.type = _type;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    public void setId(Id id) {
+        this.id = id;
+    }
+
+    public Number getValue() {
+        return value;
+    }
+
+    public void setValue(Number value) {
+        this.value = value;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
     }
 
     @Override
