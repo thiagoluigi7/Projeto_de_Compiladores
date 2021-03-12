28c4c4567d3ffd6fdd1d04a6915f7694d13c97be
Thiago Luigi <thiagoluigi7@hotmail.com>
2021-03-12 14:52:39 -0300

    :sparkles::pencil2: Adaptation of the symbol table and typo fixes.

---
+++ src/expr/SymbolTable.java
@@ -0,0 +1,38 @@
+package expr;
+
+import java.util.ArrayList;
+
+public class SymbolTable {
+
+    private ArrayList<Symbol> symbolTable;
+
+    public SymbolTable() {
+        symbolTable = new ArrayList<Symbol>();
+    }
+
+    public void add(Symble _symbol) {
+        symbolTable.add(_symbol);
+    }
+
+    public int valueof(String id) {
+        for (int i = 0; i < symbolTable.size(); i++) {
+            if (symbolTable.get(i).getId().equals(id)) {
+                return symbolTable.get(i).getValue();
+            }
+        }
+        throw new NullPointerException("Value not found.");
+    }
+
+    public boolean check(String id) {
+        for (int i = 0; i < symbolTable.size(); i++) {
+            if (symbolTable.get(i).getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        System.out.println(symbolTable.toString());
+    }
+}
